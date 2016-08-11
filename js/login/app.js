b3144406a2fc1a5539e8f00bb453d096ea5eed98
xiechao06 <xiechoa06@gmail.com>
2016-08-12 07:28:44 +0800

    partial

--- js/login/app.js
+++ js/login/app.js
@@ -1,6 +1,7 @@
 import x from '../xx.js';
 import virtualDom from 'virtual-dom';
 var h = virtualDom.h;
+import { field } from '../field.js';
 
 export const $$username = x('', 'username');
 export const $$password = x('', 'password');
@@ -8,8 +9,11 @@ export const $$password = x('', 'password');
 var $$page = x.connect(
   [$$username, $$password], 
   function (username, password) {
-    return h('.ui.grid.container', [
-      h('h1', 'login'),
+    return h('.mt4.p2.border.box.rounded.bc1.mx-auto.max-width-2', [
+      h('h3', '欢迎登陆创业电镀管理系统'),
+      h('.form', [
+  
+      ]),
     ]); 
   }
 );
