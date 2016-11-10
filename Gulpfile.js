d9ebf362f03305f42ca9f37c326bbcebd8e43b11
xc <xiechao06@gmail.com>
2016-11-10 10:10:11 +0800

    remove eval expression in generated config files

--- Gulpfile.js
+++ Gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('config', function (cb) {
       beautify(
         `
         // this file generated automatically, don't MODIFY this file
-        var _config = eval((${config}));
+        var _config = ((${config}));
         export default Object.assign(_config, {
           getPageSize(type) {
             return _config[type + 'PageSize'] || _config.pageSize;
